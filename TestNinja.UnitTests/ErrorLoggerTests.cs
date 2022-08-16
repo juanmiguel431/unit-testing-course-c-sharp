a345d939c0344d9d6a427b3de8c88d11bfa431aa
juanmiguel431 <juanmiguel431@gmail.com>
2022-08-16 09:19:49 -0400

    Testing methods that raise an Event.

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,49 @@
+using System;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        private ErrorLogger _errorLogger;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _errorLogger = new ErrorLogger();
+        }
+        
+        [Test]
+        public void Log_WhenCalled_SetLastErrorProperty()
+        {
+            _errorLogger.Log("a");
+            
+            Assert.That(_errorLogger.LastError, Is.EqualTo("a"));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
+        {
+            Assert.That(() => _errorLogger.Log(error), Throws.ArgumentNullException);
+        }
+        
+        [Test]
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            var id = Guid.Empty;
+            _errorLogger.ErrorLogged += (sender, args) =>
+            {
+                id = args;
+            };
+            
+            _errorLogger.Log("a");
+            
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+        }
+    }
+}
