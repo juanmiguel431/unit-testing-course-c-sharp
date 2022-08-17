099a398ec8b17a4ff041a550828d8b5c6d436763
juanmiguel431 <juanmiguel431@gmail.com>
2022-08-16 21:46:24 -0400

    VideoServiceTests refactored - Using Moq framework.

--- TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -1,3 +1,4 @@
+using Moq;
 using NUnit.Framework;
 using TestNinja.Mocking;
 
@@ -6,12 +7,22 @@ namespace TestNinja.UnitTests.Mocking
     [TestFixture]
     public class VideoServiceTests
     {
+        private VideoService _videoService;
+        private Mock<IFileReader> _mockFileReader;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockFileReader = new Mock<IFileReader>();
+            _videoService = new VideoService(_mockFileReader.Object);
+        }
+        
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnErrorMessage()
         {
-            var videoService = new VideoService(new FakeFileReader());
-
-            var result = videoService.ReadVideoTitle();
+            _mockFileReader.Setup(fr => fr.Read("video.txt")).Returns("");
+            
+            var result = _videoService.ReadVideoTitle();
             
             Assert.That(result, Does.Contain("error").IgnoreCase);
         }
