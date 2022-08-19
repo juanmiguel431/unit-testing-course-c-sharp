3626c5aafe78be027b62eabf326ba931a69292ef
juanmiguel431 <juanmiguel431@gmail.com>
2022-08-18 21:49:21 -0400

    InstallerHelperTests implemented.

--- TestNinja.UnitTests/Mocking/InstallerHelperTests.cs
+++ TestNinja.UnitTests/Mocking/InstallerHelperTests.cs
@@ -1,44 +1,52 @@
+using System;
+using System.Net;
+using Moq;
 using NUnit.Framework;
+using TestNinja.Mocking;
 
 namespace TestNinja.UnitTests.Mocking
 {
     [TestFixture]
     public class InstallerHelperTests
     {
+        private Mock<IWebClientWrapper> _webClientWrapperMock;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _webClientWrapperMock = new Mock<IWebClientWrapper>();
+        }
+        
         [Test]
         public void DownloadInstaller_WebException_ReturnsFalse()
         {
+            _webClientWrapperMock.Setup(c => c.Download(It.IsAny<string>(), It.IsAny<string>())).Throws<WebException>();
+                
+            var installerHelper = new InstallerHelper(_webClientWrapperMock.Object);
+
+            var result = installerHelper.DownloadInstaller("", "", "");
             
+            Assert.That(result, Is.EqualTo(false));
         }
         
         [Test]
-        public void DownloadInstaller_WebGenericException_ThrowsTheException()
+        public void DownloadInstaller_WhenGenericException_ThrowsTheException()
         {
+            _webClientWrapperMock.Setup(c => c.Download(It.IsAny<string>(), It.IsAny<string>())).Throws<Exception>();
+                
+            var installerHelper = new InstallerHelper(_webClientWrapperMock.Object);
             
+            Assert.That(() => installerHelper.DownloadInstaller("", "", ""), Throws.TypeOf<Exception>());
         }
-        
+
         [Test]
         public void DownloadInstaller_FileDownloaded_ReturnsTrue()
         {
+            var installerHelper = new InstallerHelper(_webClientWrapperMock.Object);
+
+            var result = installerHelper.DownloadInstaller("", "", "");
             
-        }
-        
-        [Test]
-        public void DownloadInstaller_FileDownloaded_SetupDestinationFilePropertyIsNotNullOrEmpty()
-        {
-            
-        }
-        
-        [Test]
-        [TestCase(null, null)]
-        [TestCase("a", null)]
-        [TestCase(null, "a")]
-        [TestCase("", "")]
-        [TestCase("a", "")]
-        [TestCase("", "b")]
-        public void DownloadInstaller_AnyParameterNullOrEmpty_ReturnsFalse(string customerName, string installerName)
-        {
-            
+            Assert.That(result, Is.EqualTo(true));
         }
     }
 }
