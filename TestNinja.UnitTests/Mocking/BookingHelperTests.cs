5cf0b4745137ce6df59e88ca0cffae68e4864ebc
juanmiguel431 <juanmiguel431@gmail.com>
2022-08-20 11:21:16 -0400

    OverlappingBookingsExist_StatusIsCanceled_ReturnsEmptyString implemented.

--- TestNinja.UnitTests/Mocking/BookingHelperTests.cs
+++ TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using TestNinja.Mocking;
 
 namespace TestNinja.UnitTests.Mocking
 {
@@ -8,25 +9,24 @@ namespace TestNinja.UnitTests.Mocking
         [Test]
         public void OverlappingBookingsExist_StatusIsCanceled_ReturnsEmptyString()
         {
+            var result = BookingHelper.OverlappingBookingsExist(new Booking() { Status = "Cancelled" });
             
+            Assert.That(result, Is.EqualTo(string.Empty));
         }
-        
+
         [Test]
         public void OverlappingBookingsExist_NoOverlappingRecordFound_ReturnsEmptyString()
         {
-            
         }
-        
+
         [Test]
         public void OverlappingBookingsExist_WhenOverlappingRecordFound_ReturnsReferenceProperty()
         {
-            
         }
-        
+
         [Test]
         public void OverlappingBookingsExist_WhenMoreThanOneOverlappingRecordFound_ReturnsReferencePropertyOfTheFirstOccurrence()
         {
-            
         }
     }
 }
