3b4e77bf9af04ade627431eae3c1918fd97d474a
juanmiguel431 <juanmiguel431@gmail.com>
2022-08-20 11:11:29 -0400

    BookingHelperTests - OverlappingBookingsExist test definitions added

---
+++ TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class BookingHelperTests
+    {
+        [Test]
+        public void OverlappingBookingsExist_StatusIsCanceled_ReturnsEmptyString()
+        {
+            
+        }
+        
+        [Test]
+        public void OverlappingBookingsExist_NoOverlappingRecordFound_ReturnsEmptyString()
+        {
+            
+        }
+        
+        [Test]
+        public void OverlappingBookingsExist_WhenOverlappingRecordFound_ReturnsReferenceProperty()
+        {
+            
+        }
+        
+        [Test]
+        public void OverlappingBookingsExist_WhenMoreThanOneOverlappingRecordFound_ReturnsReferencePropertyOfTheFirstOccurrence()
+        {
+            
+        }
+    }
+}
