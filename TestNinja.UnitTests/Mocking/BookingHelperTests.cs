4d5b46c2e1d8d51e1508ddda0a29580c98384ae5
juanmiguel431 <juanmiguel431@gmail.com>
2022-08-20 11:44:08 -0400

    OverlappingBookingsExist_WhenOverlappingRecordFound_ReturnsReferenceProperty implemented.

--- TestNinja.UnitTests/Mocking/BookingHelperTests.cs
+++ TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -7,33 +7,39 @@ namespace TestNinja.UnitTests.Mocking
     [TestFixture]
     public class BookingHelperTests
     {
+        private Mock<IBookingRepository> _bookingRepositoryMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _bookingRepositoryMock = new Mock<IBookingRepository>();
+            BookingHelper.BookingRepository = _bookingRepositoryMock.Object;
+        }
+
         [Test]
         public void OverlappingBookingsExist_StatusIsCanceled_ReturnsEmptyString()
         {
-            var result = BookingHelper.OverlappingBookingsExist(new Booking() { Status = "Cancelled" });
-            
+            var result = BookingHelper.OverlappingBookingsExist(new Booking { Status = "Cancelled" });
+
             Assert.That(result, Is.EqualTo(string.Empty));
         }
 
         [Test]
         public void OverlappingBookingsExist_NoOverlappingRecordFound_ReturnsEmptyString()
         {
-            var bookingRepositoryMock = new Mock<IBookingRepository>();
-            BookingHelper.BookingRepository = bookingRepositoryMock.Object;
-            
             var result = BookingHelper.OverlappingBookingsExist(new Booking());
-            
+
             Assert.That(result, Is.EqualTo(string.Empty));
         }
 
         [Test]
         public void OverlappingBookingsExist_WhenOverlappingRecordFound_ReturnsReferenceProperty()
         {
-        }
+            _bookingRepositoryMock.Setup(r => r.GetFirstOrDefaultOverlappingBooking(It.IsAny<Booking>())).Returns(new Booking() { Reference = "a" });
 
-        [Test]
-        public void OverlappingBookingsExist_WhenMoreThanOneOverlappingRecordFound_ReturnsReferencePropertyOfTheFirstOccurrence()
-        {
+            var result = BookingHelper.OverlappingBookingsExist(new Booking());
+            
+            Assert.That(result, Is.EqualTo("a"));
         }
     }
 }
